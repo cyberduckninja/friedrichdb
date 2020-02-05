68814414322410baff2872a9694ba6a5c8dc1c3c
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-02-05 10:22:36 +0300

    work in table

--- header/friedrichdb/core/table.hpp
+++ header/friedrichdb/core/table.hpp
@@ -12,17 +12,17 @@ enum class table_type {
 
 
 struct field final {
-    std::string name_;
+    boost::interprocess::string name_;
     field_base base_;
 };
 
-using row_t = std::vector<field>;
-using storage_base_t = boost::container::stable_vector<std::vector<row_t>>;
+using row_t = boost::interprocess::vector<field>;
+using storage_base_t = boost::container::stable_vector<boost::interprocess::vector<row_t>>;
 
 
-struct schema final {
+struct schema_t final {
     table_type table_type_;
-    std::vector<field_type> storage_;
+    boost::interprocess::vector<field_type> storage_;
 };
 
 /// base storage in memory / shm / gpu
@@ -30,7 +30,7 @@ class table final {
 public:
     using iterator = storage_base_t::iterator ;
 
-    table(const schema& current_schema):schema_(current_schema){}
+    table(const schema_t& current_schema):schema_(current_schema){}
 
     row_t& row(std::size_t index ){
 
@@ -44,7 +44,7 @@ public:
 
     }
 
-    const schema& schema() const { return schema_; }
+    const schema_t& schema() const { return schema_; }
 
     /// \brief Return a column by index
     virtual std::shared_ptr<ChunkedArray> column(int i) const = 0;
@@ -59,7 +59,7 @@ public:
     std::vector<std::shared_ptr<Field>> fields() const;
 
 
-    virtual std::shared_ptr<Table> Slice(int64_t offset, int64_t length) const = 0;
+     std::shared_ptr<Table> Slice(int64_t offset, int64_t length) const = 0;
 
     std::shared_ptr<Table> Slice(int64_t offset) const { return Slice(offset, num_rows_); }
 
@@ -70,15 +70,15 @@ public:
     }
 
 
-    virtual Status RemoveColumn(int i, std::shared_ptr<Table>* out) const = 0;
+     Status RemoveColumn(int i, std::shared_ptr<Table>* out) const = 0;
 
 
-    virtual Status AddColumn(int i, std::shared_ptr<Field> field_arg,
+     Status AddColumn(int i, std::shared_ptr<Field> field_arg,
                              std::shared_ptr<ChunkedArray> column,
                              std::shared_ptr<Table>* out) const = 0;
 
 
-    virtual Status SetColumn(int i, std::shared_ptr<Field> field_arg,
+     Status SetColumn(int i, std::shared_ptr<Field> field_arg,
                              std::shared_ptr<ChunkedArray> column,
                              std::shared_ptr<Table>* out) const = 0;
 
@@ -86,16 +86,16 @@ public:
     std::vector<std::string> ColumnNames() const;
 
 
-    virtual Status Validate() const = 0;
+     Status Validate() const = 0;
 
-    virtual Status ValidateFull() const = 0;
+     Status ValidateFull() const = 0;
 
     int num_columns() const { return schema_.storage_.size(); }
 
     int64_t num_rows() const { return num_rows_; }
 
 private:
-    schema schema_;
+    schema_t schema_;
     storage_base_t storage_;
     int64_t num_rows_
 };

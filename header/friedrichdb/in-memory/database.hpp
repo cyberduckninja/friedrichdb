362a71900c97b972f4d57c2e58819921cdfe04e7
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-03-04 22:20:30 +0300

    fix

--- header/friedrichdb/in-memory/database.hpp
+++ header/friedrichdb/in-memory/database.hpp
@@ -23,7 +23,9 @@ namespace friedrichdb { namespace in_memory {
 
         class database final {
         public:
-            database(const core::database_constructor_options &options) {}
+            database(const core::database_constructor_options &options) {
+
+            }
 
             auto remove(const core::collection_remove_options &options) -> void {
 
@@ -39,8 +41,9 @@ namespace friedrichdb { namespace in_memory {
                 }
             }
 
-            auto create(const core::collection_create_options &options) -> view_collection {
-                storage_.emplace_back(new collection());
+            template <typename ...Args>
+            auto create(const core::collection_create_options &options,Args... args) -> view_collection {
+                storage_.emplace_back(new collection(std::forward<Args>(args...)));
                 auto result = name_to_idx_.emplace(options.name_, storage_.size());
                 auto it = storage_.begin();
                 std::advance(it,result.first->second);
@@ -60,6 +63,20 @@ namespace friedrichdb { namespace in_memory {
                     return result->get();
                 }
 
+            }
+
+            auto all_names() const -> std::set<std::string> {
+                std::set<std::string> tmp;
+
+                for(const auto&i:name_to_idx_ ){
+                    tmp.emplace(i.first);
+                }
+
+                return tmp;
+            }
+
+            auto size() const -> std::size_t {
+                return storage_.size();
             }
 
         private:

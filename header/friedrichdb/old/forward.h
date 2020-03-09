5c65dcc99eda429ff13796a96331facaeb3ff296
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-03-09 14:34:17 +0300

    clean old

--- header/friedrichdb/old/forward.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace friedrichdb {
-    struct abstract_database;
-    struct abstract_table;
-    using id_t = std::size_t ;
-
-}
--- sourcer/fake_file_storage.cpp
+++
@@ -1,8 +0,0 @@
-#include <friedrichdb/fake_file_storage.hpp>
-
-namespace friedrichdb {
-    file_storage_fake::file_storage_fake() : abstract_database(storage_type ::disk) {}
-
-    auto file_storage_fake::apply(query&&) -> output_query {}
-
-}
